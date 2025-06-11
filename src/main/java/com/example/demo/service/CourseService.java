6554b5c11529bdf21ce797a37d4100da0d0ced43
Ahmed Ashraf <ahmed_ashraf@sme.com>
2025-06-11 08:40:06 +0300

    Courses 1

--- src/main/java/com/example/demo/service/CourseService.java
+++ src/main/java/com/example/demo/service/CourseService.java
@@ -1,7 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Course;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @Service
 public interface CourseService {
+
+    Course saveCourse (Course course);
+    List<Course> saveAllCourses (List<Course> course);
+    Page<Course> getAllCourses (Pageable pageable);
 }
